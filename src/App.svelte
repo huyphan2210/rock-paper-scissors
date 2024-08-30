fb00490a77ffcb386406d6e77143f49a37445b11
Huy Phan <huy.phan2210@outlook.com>
2024-08-30 13:25:30 +0700

    Update App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,7 @@
 <script lang="ts">
+  import GameSection from "./sections/GameSection/GameSection.svelte";
   import ScoreSection from "./sections/ScoreSection/ScoreSection.svelte";
 </script>
 
 <ScoreSection />
-
-<style>
-</style>
+<GameSection />
