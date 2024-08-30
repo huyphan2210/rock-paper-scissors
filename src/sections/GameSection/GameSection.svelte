314ed79021656c3da31bc57ac52ac13614221cda
Huy Phan <huy.phan2210@outlook.com>
2024-08-30 13:25:25 +0700

    Create GameSection.svelte

---
+++ src/sections/GameSection/GameSection.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+  import pentagonSvg from "../../assets/images/bg-pentagon.svg";
+</script>
+
+<section id="game-section">
+  <div class="pentagon">
+    <img src={pentagonSvg} loading="lazy" alt="Pentagon" />
+  </div>
+</section>
+
+<style lang="scss" scoped>
+  #game-section {
+    width: 100%;
+    height: 50vh;
+    margin-top: 10vh;
+    display: grid;
+    place-items: center;
+  }
+
+  .pentagon {
+    img {
+      width: 100%;
+    }
+  }
+</style>
