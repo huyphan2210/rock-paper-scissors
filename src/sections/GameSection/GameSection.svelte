166d035c386e2ef09a9eebb74d1df7db3eb10164
Huy Phan <huy.phan2210@outlook.com>
2024-09-04 16:01:09 +0700

    Add GameIcon to GameSecion

--- src/sections/GameSection/GameSection.svelte
+++ src/sections/GameSection/GameSection.svelte
@@ -1,14 +1,54 @@
 <script lang="ts">
   import pentagonSvg from "../../assets/images/bg-pentagon.svg";
+  import type { GameIconProps } from "./components/GameIcon/GameIcon.svelte";
+  import iconRock from "../../assets/images/icon-rock.svg";
+  import iconPaper from "../../assets/images/icon-paper.svg";
+  import iconScissors from "../../assets/images/icon-scissors.svg";
+  import iconLizard from "../../assets/images/icon-lizard.svg";
+  import iconSpock from "../../assets/images/icon-spock.svg";
+  import GameIcon from "./components/GameIcon/GameIcon.svelte";
+
+  const gameIcons: GameIconProps[] = [
+    {
+      iconImage: iconRock,
+      iconName: "Rock",
+    },
+    {
+      iconImage: iconPaper,
+      iconName: "Paper",
+    },
+    {
+      iconImage: iconScissors,
+      iconName: "Scissors",
+    },
+    {
+      iconImage: iconLizard,
+      iconName: "Lizard",
+    },
+    {
+      iconImage: iconSpock,
+      iconName: "Spock",
+    },
+  ];
 </script>
 
 <section id="game-section">
   <div class="pentagon">
     <img src={pentagonSvg} loading="lazy" alt="Pentagon" />
+    {#each gameIcons as { iconImage, iconName }}
+      <GameIcon {iconImage} {iconName} />
+    {/each}
   </div>
 </section>
 
 <style lang="scss" scoped>
+  $base-scale: 80%;
+  @mixin responsive-scale($scale-factor) {
+    @media (min-width: 1024px) {
+      transform: scale($scale-factor * 1.2);
+    }
+  }
+
   #game-section {
     width: 100%;
     height: 50vh;
@@ -18,6 +58,9 @@
   }
 
   .pentagon {
+    position: relative;
+    transform: scale($base-scale);
+    @include responsive-scale(1);
     img {
       width: 100%;
     }
