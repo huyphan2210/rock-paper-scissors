4399891fe1f9cb09860de3c1e36a47d6df39e109
Huy Phan <huy.phan2210@outlook.com>
2024-09-06 11:48:30 +0700

    Add rules button to show Rules modal

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,47 @@
 <script lang="ts">
   import GameSection from "./sections/GameSection/GameSection.svelte";
+  import RulesSection from "./sections/RulesSection/RulesSection.svelte";
   import ScoreSection from "./sections/ScoreSection/ScoreSection.svelte";
+
+  let isRulesOn = false;
+
+  const toggleRules = () => {
+    isRulesOn = !isRulesOn;
+  };
 </script>
 
 <ScoreSection />
 <GameSection />
+{#if isRulesOn}
+  <RulesSection closeRules={toggleRules} />
+{/if}
+<button on:click={toggleRules} type="button">RULES</button>
+
+<style lang="scss" scoped>
+  button {
+    position: absolute;
+    left: 50%;
+    bottom: 2rem;
+    transform: translateX(-50%);
+    padding: 1rem 2rem;
+    background-color: transparent;
+    border: solid 0.125rem var(--header-outline);
+    border-radius: 0.5rem;
+    color: white;
+    letter-spacing: 0.125rem;
+    &:hover {
+      cursor: pointer;
+      background-color: white;
+      border-color: white;
+      color: var(--dark-text);
+    }
+  }
+
+  @media screen and (min-width: 1024px) {
+    button {
+      right: 2rem;
+      left: unset;
+      transform: none;
+    }
+  }
+</style>
