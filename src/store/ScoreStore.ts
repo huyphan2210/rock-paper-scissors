7f59b03866671efb67b09040e7bf123f0ba38fd0
Huy Phan <huy.phan2210@outlook.com>
2024-09-06 11:47:23 +0700

    Update ScoreStore.ts

--- src/store/ScoreStore.ts
+++ src/store/ScoreStore.ts
@@ -1,7 +1,31 @@
 import { writable } from "svelte/store";
+export enum Choice {
+  Rock = "Rock",
+  Paper = "Paper",
+  Scissors = "Scissors",
+  Lizard = "Lizard",
+  Spock = "Spock",
+}
 
 export const score = writable(0);
 
+export const playerChoice = writable<Choice | undefined>();
+export const botChoice = writable<Choice | undefined>();
+
 export const increaseScore = () => {
   score.update((lastScore) => lastScore + 1);
+};
+
+export const setPlayerChoice = (choice?: Choice) => {
+  playerChoice.update(() => choice);
+};
+
+export const setBotChoice = () => {
+  botChoice.update(getRandomChoice);
+};
+
+const getRandomChoice = () => {
+  const values = Object.values(Choice);
+  const randomIndex = Math.floor(Math.random() * values.length);
+  return values[randomIndex] as Choice;
 };
