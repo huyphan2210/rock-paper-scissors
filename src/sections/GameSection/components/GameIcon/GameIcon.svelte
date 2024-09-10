544e9f7caff87acc003a24dddd104f0cf36cd0c9
Huy Phan <huy.phan2210@outlook.com>
2024-09-10 11:41:04 +0700

    Update GameIcon for the house

--- src/sections/GameSection/components/GameIcon/GameIcon.svelte
+++ src/sections/GameSection/components/GameIcon/GameIcon.svelte
@@ -1,6 +1,6 @@
 <script context="module" lang="ts">
   export interface GameIconProps {
-    iconImage: string;
+    iconImage?: string;
     iconName?: Choice;
   }
 
@@ -11,18 +11,26 @@
 </script>
 
 <script lang="ts">
-  import { Choice, setPlayerChoice } from "../../../../store/ScoreStore";
+  import { Choice, playerChoice } from "../../../../store/ScoreStore";
 
-  export let iconImage: string = defaultProps.iconImage;
+  export let iconImage: string | undefined = defaultProps.iconImage;
   export let iconName = defaultProps.iconName;
+  export let isTheHouse: boolean;
 
-  const handlePlayerChoice = () => {
-    //To do: add animation
-    setPlayerChoice(iconName);
-  };
+  export let handlePlayerChoice = (iconName?: Choice) => {};
 </script>
 
-<button on:click={handlePlayerChoice} type="button" title={iconName}>
+<button
+  class:chosen={!isTheHouse && $playerChoice && $playerChoice === iconName}
+  class:invisible={!isTheHouse && $playerChoice && $playerChoice !== iconName}
+  class:the-house-choice={isTheHouse}
+  class:the-house-reveal={isTheHouse && $playerChoice}
+  on:click={() => {
+    handlePlayerChoice(iconName);
+  }}
+  type="button"
+  title={iconName}
+>
   <img src={iconImage} loading="lazy" alt={iconName} />
 </button>
 
@@ -58,6 +66,42 @@
 
     &:hover {
       cursor: pointer;
+    }
+
+    &.chosen {
+      position: fixed;
+      scale: 1.5;
+      left: unset;
+      right: 100%;
+      top: 50%;
+      animation-play-state: paused;
+    }
+
+    &.invisible {
+      animation-play-state: paused;
+      opacity: 0;
+      cursor: none;
+      pointer-events: none;
+    }
+
+    &.the-house-choice {
+      position: fixed;
+      left: 100%;
+      top: 0;
+      cursor: none;
+      pointer-events: none;
+      animation: none;
+
+      img {
+        opacity: 0;
+      }
+    }
+
+    &.the-house-reveal {
+      scale: 1.5;
+      img {
+        opacity: 1;
+      }
     }
   }
 
