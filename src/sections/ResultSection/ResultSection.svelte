e020994737d38ae4787a565bc6a5c7d997ce1757
Huy Phan <huy.phan2210@outlook.com>
2024-09-13 11:44:57 +0700

    Style result declartion

--- src/sections/ResultSection/ResultSection.svelte
+++ src/sections/ResultSection/ResultSection.svelte
@@ -1,7 +1,30 @@
 <script lang="ts">
-  import { playerChoice } from "../../store/ScoreStore";
-  import { gameIcons } from "../../utilities/gameUtil";
+  import { onMount } from "svelte";
+  import {
+    increaseScore,
+    playerChoice,
+    reset,
+    setTheHouseChoice,
+    theHouseChoice,
+  } from "../../store/ScoreStore";
+  import { calculateResult, gameIcons } from "../../utilities/gameUtil";
   import GameIcon from "../GameSection/components/GameIcon/GameIcon.svelte";
+
+  let result: number | undefined;
+
+  onMount(() => {
+    const interval = setInterval(() => {
+      setTheHouseChoice();
+    }, 300);
+
+    setTimeout(() => {
+      clearInterval(interval);
+      result = calculateResult($playerChoice!, $theHouseChoice!);
+      if (result === 1) {
+        increaseScore();
+      }
+    }, 5000);
+  });
 </script>
 
 <section id="result">
@@ -12,10 +35,22 @@
       ?.iconImage}
     isTheHouse={false}
   />
+  <div class="result-declaration" class:reveal={result !== undefined}>
+    {#if result === -1}
+      <h2>You lose</h2>
+    {:else if result === 0}
+      <h2>Draw</h2>
+    {:else if result === 1}
+      <h2>You win</h2>
+    {:else}
+      <h2>Placeholder</h2>
+    {/if}
+    <button type="button" on:click={reset}>Play again</button>
+  </div>
   <GameIcon
     isPlayer={false}
-    iconName={$playerChoice}
-    iconImage={gameIcons.find((icon) => icon.iconName === $playerChoice)
+    iconName={$theHouseChoice}
+    iconImage={gameIcons.find((icon) => icon.iconName === $theHouseChoice)
       ?.iconImage}
     isTheHouse
   />
@@ -23,24 +58,63 @@
 
 <style lang="scss">
   #result {
+    position: relative;
     flex: 1;
     width: 100%;
     gap: 2rem;
     max-width: 60rem;
-    animation: reveal 2s;
+    animation: reveal 1s;
     display: flex;
     align-items: center;
     justify-content: space-between;
+
+    .result-declaration {
+      position: absolute;
+      bottom: 7rem;
+      left: 50%;
+      transform: translateX(-50%);
+      width: 0;
+      overflow: hidden;
+      text-transform: uppercase;
+      color: white;
+      transition: 1s;
+      h2 {
+        text-align: center;
+        font-size: clamp(3rem, 3.5vw, 3.5rem);
+        white-space: nowrap;
+        letter-spacing: 0.1rem;
+      }
+
+      button {
+        display: block;
+        background-color: white;
+        font-family: "Barlow Semi Condensed", sans-serif;
+        padding: 1rem 2rem;
+        border: none;
+        width: 100%;
+        text-transform: uppercase;
+        color: var(--dark-text);
+        font-weight: 500;
+        white-space: nowrap;
+        font-size: clamp(1.5rem, 2vw, 2rem);
+        letter-spacing: 0.1rem;
+        border-radius: 0.5rem;
+
+        &:hover {
+          cursor: pointer;
+          color: var(--rock-gradient-1);
+        }
+      }
+
+      &.reveal {
+        width: 100%;
+      }
+    }
   }
 
   @keyframes reveal {
     0% {
-      display: none;
       opacity: 0;
-    }
-    50% {
-      opacity: 0;
-      display: flex;
     }
     100% {
       opacity: 1;
@@ -51,6 +125,14 @@
     #result {
       justify-content: center;
       gap: 5rem;
+      .result-declaration {
+        position: static;
+        transform: none;
+
+        &.reveal {
+          width: 25%;
+        }
+      }
     }
   }
 </style>
