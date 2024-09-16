b9f21e613e7d9dda663e9b398a84f9ac11b0f9ea
Huy Phan <huy.phan2210@outlook.com>
2024-09-16 12:05:58 +0700

    Update ResultSection.svelte
    Update the result declartion

--- src/sections/ResultSection/ResultSection.svelte
+++ src/sections/ResultSection/ResultSection.svelte
@@ -28,13 +28,34 @@
 </script>
 
 <section id="result">
-  <GameIcon
-    isPlayer
-    iconName={$playerChoice}
-    iconImage={gameIcons.find((icon) => icon.iconName === $playerChoice)
-      ?.iconImage}
-    isTheHouse={false}
-  />
+  <div class="game-icons-display">
+    <GameIcon
+      isPlayer
+      iconName={$playerChoice}
+      iconImage={gameIcons.find((icon) => icon.iconName === $playerChoice)
+        ?.iconImage}
+      isTheHouse={false}
+    />
+    <div class="result-declaration desktop" class:reveal={result !== undefined}>
+      {#if result === -1}
+        <h2>You lose</h2>
+      {:else if result === 0}
+        <h2>Draw</h2>
+      {:else if result === 1}
+        <h2>You win</h2>
+      {:else}
+        <h2>Placeholder</h2>
+      {/if}
+      <button type="button" on:click={reset}>Play again</button>
+    </div>
+    <GameIcon
+      isPlayer={false}
+      iconName={$theHouseChoice}
+      iconImage={gameIcons.find((icon) => icon.iconName === $theHouseChoice)
+        ?.iconImage}
+      isTheHouse
+    />
+  </div>
   <div class="result-declaration" class:reveal={result !== undefined}>
     {#if result === -1}
       <h2>You lose</h2>
@@ -47,46 +68,43 @@
     {/if}
     <button type="button" on:click={reset}>Play again</button>
   </div>
-  <GameIcon
-    isPlayer={false}
-    iconName={$theHouseChoice}
-    iconImage={gameIcons.find((icon) => icon.iconName === $theHouseChoice)
-      ?.iconImage}
-    isTheHouse
-  />
 </section>
 
 <style lang="scss">
   #result {
-    position: relative;
     flex: 1;
     width: 100%;
-    gap: 2rem;
     max-width: 60rem;
     animation: reveal 1s;
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
+    display: grid;
+    place-items: center;
 
+    .game-icons-display {
+      display: flex;
+      align-items: center;
+      justify-content: space-around;
+      gap: 2rem;
+    }
     .result-declaration {
-      position: absolute;
-      bottom: 7rem;
-      left: 50%;
-      transform: translateX(-50%);
       width: 0;
       overflow: hidden;
       text-transform: uppercase;
       color: white;
       transition: 1s;
+      &.desktop {
+        display: none;
+      }
       h2 {
-        text-align: center;
+        display: flex;
+        justify-content: center;
         font-size: clamp(3rem, 3.5vw, 3.5rem);
         white-space: nowrap;
         letter-spacing: 0.1rem;
       }
 
       button {
-        display: block;
+        display: flex;
+        justify-content: center;
         background-color: white;
         font-family: "Barlow Semi Condensed", sans-serif;
         padding: 1rem 2rem;
@@ -123,14 +141,19 @@
 
   @media screen and (min-width: 720px) {
     #result {
-      justify-content: center;
-      gap: 5rem;
+      place-items: unset;
+      .game-icons-display {
+        justify-content: center;
+        gap: 5rem;
+      }
       .result-declaration {
-        position: static;
-        transform: none;
+        display: none;
 
-        &.reveal {
-          width: 25%;
+        &.desktop {
+          display: block;
+          &.reveal {
+            width: 25%;
+          }
         }
       }
     }
